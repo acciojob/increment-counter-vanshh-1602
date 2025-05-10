33f77a730969d9babc9dc43f2d81bc2a543c2f82
Vansh Bhadoria <115222279+vanshh-1602@users.noreply.github.com>
2025-05-10 20:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const c = document.getElementById('counter');
 const btn = document.getElementById('btn');
-let count = parseInt(c.innerText);
+let count = parseInt(c.innerText, 10);
 
 
 btn.addEventListener('click', () =>{
