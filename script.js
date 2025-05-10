295757707e4180cc8a36aa761b210119a4d65017
Vansh Bhadoria <115222279+vanshh-1602@users.noreply.github.com>
2025-05-10 19:55:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const c = document.getElementById('counter');
+const btn = document.getElementById('btn');
+let count = parseInt(c.innerText);
+
+
+btn.addEventListener('click', () =>{
+	count++;
+	alert(count);
+	c.innerText = count;
+	
+});
